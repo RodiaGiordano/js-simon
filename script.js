5a23bdc9039afb0b36d76d9b0e4655bf70f14470
RodiaGiordano <rodiagiordano93@gmail.com>
2023-08-23 15:11:25 +0200

    feat: calc secondsRemaining

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("JS logic OK")
+
+const dateTomorrow = new Date (2023, 7, 24, 9, 30, 0 );
+
+const dateRightNow = new Date ();
+
+console.log("domani 9:30: " + dateTomorrow)
+console.log("ora: " + dateRightNow);
+
+const tomorrow = dateTomorrow.getTime();
+console.log("ms domani: " + tomorrow);
+
+const rightNow = dateRightNow.getTime();
+console.log("ms ora: " + rightNow)
+
+
+const timeLimit = tomorrow - rightNow;
+console.log(timeLimit)
+console.log(typeof timeLimit)
+
+const secondsRemaining = Math.floor(timeLimit / 1000);
+console.log(secondsRemaining)
+
+
