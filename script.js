48093912f4947670ae76efdea163cddc5f6e0214
RodiaGiordano <rodiagiordano93@gmail.com>
2023-08-23 16:01:26 +0200

    feat: add startupdate after stop

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
 const buttonStop = document.getElementById('stop')
+const buttonStart = document.getElementById('start')
 const countDownDisplay = document.getElementById('time')
 
 
 const dateTomorrow = new Date (2023, 7, 24, 9, 30, 0 );
+// console.log("domani 9:30: " + dateTomorrow)
 
-const dateRightNow = new Date ();
-
-console.log("domani 9:30: " + dateTomorrow)
-console.log("ora: " + dateRightNow);
-
-const tomorrow = dateTomorrow.getTime();
-console.log("ms domani: " + tomorrow);
-
-const rightNow = dateRightNow.getTime();
-console.log("ms ora: " + rightNow)
-
-
-let timeLimit = tomorrow - rightNow;
-console.log(timeLimit)
-console.log(typeof timeLimit)
-
-let secondsRemaining = Math.floor(timeLimit / 1000);
-console.log(secondsRemaining)
 
 let timeStop = false;
 
-buttonStop.addEventListener('click', function(){
+buttonStart.addEventListener('click', function(){
 
-    timeStop = true;
-})
+    const dateRightNow = new Date ();
+
+    // console.log("ora: " + dateRightNow);
+
+    const tomorrow = dateTomorrow.getTime();
+    // console.log("ms domani: " + tomorrow);
+
+    const rightNow = dateRightNow.getTime();
+    // console.log("ms ora: " + rightNow)
+
+
+    let timeLimit = tomorrow - rightNow;
+    // console.log(timeLimit)
+    // console.log(typeof timeLimit)
+
+    let secondsRemaining = Math.floor(timeLimit / 1000);
+    // console.log(secondsRemaining)
+
+    
+
+    
+    timeStop = false;
     
     const timeRemaining = setInterval(function () {
-        
-        secondsRemaining--
-        console.log(secondsRemaining)
 
+        secondsRemaining--
+
+        console.log("con stop: " + secondsRemaining)
+    
         if(timeStop){
             clearInterval(timeRemaining)
         }
         
     }, 1000);
+    
+})
+
+
+
+
+buttonStop.addEventListener('click', function(){
+
+    timeStop = true;
+})
+
+
+
+
+
+    
     
 
 
