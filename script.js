58ad7d2f68310a12a79de3a6c10c87ab34cd2143
RodiaGiordano <rodiagiordano93@gmail.com>
2023-08-23 15:47:10 +0200

    feat: add countDown and stop

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const buttonStop = document.getElementById('stop')
+const countDownDisplay = document.getElementById('time')
+
 
 const dateTomorrow = new Date (2023, 7, 24, 9, 30, 0 );
 
@@ -13,11 +16,31 @@ const rightNow = dateRightNow.getTime();
 console.log("ms ora: " + rightNow)
 
 
-const timeLimit = tomorrow - rightNow;
+let timeLimit = tomorrow - rightNow;
 console.log(timeLimit)
 console.log(typeof timeLimit)
 
-const secondsRemaining = Math.floor(timeLimit / 1000);
+let secondsRemaining = Math.floor(timeLimit / 1000);
 console.log(secondsRemaining)
+
+let timeStop = false;
+
+buttonStop.addEventListener('click', function(){
+
+    timeStop = true;
+})
+    
+    const timeRemaining = setInterval(function () {
+        
+        secondsRemaining--
+        console.log(secondsRemaining)
+
+        if(timeStop){
+            clearInterval(timeRemaining)
+        }
+        
+    }, 1000);
+    
+
 
 
